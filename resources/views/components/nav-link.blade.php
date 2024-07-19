bda8f70c833320e0e92c96e0931000fbb9b694b4
Alexandre Silva <asilva1604@outlook.com>
2024-07-19 13:36:09 +0100

    day 5 and respective homework completed

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,7 @@
-<a {{$attributes}}>{{$slot}}</a>
+@props(['active' => false, 'type' => 'a'])
+
+@if($type === 'a')
+    <a {{$attributes}} class="rounded-md px-3 py-2 text-sm font-medium {{$active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}" aria-current={{$active ? 'page' : 'false'}}>{{$slot}}</a>
+@else
+    <button {{$attributes}} class="rounded-md px-3 py-2 text-sm font-medium {{$active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}" aria-current={{$active ? 'page' : 'false'}}>{{$slot}}</button>
+@endif
